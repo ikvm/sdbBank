fdca2788e08b687296ec632e23c45ef0998d7097
dell <dell@DELL-PC>
2017-04-11 16:46:28 +0800

    快付

--- Pingan/Controllers/sdbController.cs
+++ Pingan/Controllers/sdbController.cs
@@ -10,22 +10,58 @@ namespace Pingan.Controllers
 {
     public class sdbController : Controller
     {
-        // GET: sdb
-        public ActionResult Index()
+        /// <summary>
+        /// 查询余额
+        /// </summary>
+        /// <param name="accountNo"></param>
+        /// <returns></returns>
+        public ActionResult Index( string accountNo = "11002923034501" )
         {
-            string accountNo = "11002923034501";
-            var oResult =  PinganQuery.Account4001Query(accountNo);
-            return Content(oResult.ToString());
+        
+            var oResult =  PinganQuery.YQ4001NormalQuery(accountNo);
+            return Content(oResult );
             //return View(oResult);
         }
 
-        public ActionResult sResultNormal()
+
+        /// <summary>
+        /// 系统探测
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult sys()
         {
             var sResultNormal = PinganQuery.S001NormalQuery();
             return Content(sResultNormal);
 
         }
 
+        /// <summary>
+        /// 跨行快付
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult KHKF03(string InAcctNo= "6221558812340000", string InAcctName= "互联网", string TranAmount="5.59")
+        {
+            var oResult = PinganQuery.KHKF03(  InAcctNo,   InAcctName,   TranAmount);
+            return Content(oResult);
+        }
+
+
+
+        /// <summary>
+        /// 跨行快付查询  3.4 单笔付款结果查询  http://localhost:4113/sdb/KHKF04
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult KHKF04(string OrderNumber = "ZXLKF0320170411TV001")
+        {
+            //传订单号
+            var oResult = PinganQuery.KHKF04(OrderNumber);
+            return Content(oResult);
+        }
+
+
+
+
+        //使用post查询_无效
         public ActionResult sResult()
         {
             var sResult = PinganQuery.S001Query();
--- sdbBank/PinganQuery.cs
+++ sdbBank/PinganQuery.cs
@@ -23,6 +23,11 @@ namespace sdbBank
 
       private static string S001XML = "<?xml version=\"1.0\" encoding=\"GBK\"?><Result></Result>";
 
+
+        /// <summary>
+        /// 系统探测
+        /// </summary>
+        /// <returns></returns>
         public static string S001NormalQuery()
         {
             string postParams = "{0}";
@@ -37,7 +42,125 @@ namespace sdbBank
         }
 
 
-        //3.1 系统状态探测,没有用封装的post提交
+        /// <summary>
+        /// 余额查询
+        /// </summary>
+        /// <returns></returns>
+        public static string YQ4001NormalQuery(string Account)
+        {
+         
+            string postParams = "{0}";
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<?xml version=\"1.0\" encoding=\"GBK\" ?><Result><Account>");
+            sb.Append(Account);
+            sb.Append("</Account><CcyCode>");
+            sb.Append("RMB");
+            sb.Append("</CcyCode></Result>");
+            string S4001XML = string.Format(postParams, sb.ToString());
+ 
+            var str = YQHelp.asemblyYQPackets(YQCode, "4001", S4001XML);
+ 
+          var cc = PAHelper.NcPost(apiUrl, str);
+            return cc;
+        }
+
+
+        /// <summary>
+        /// 跨行快付  单笔
+        /// </summary>
+        /// <param name="InAcctNo"></param>
+        /// <param name="InAcctName"></param>
+        /// <param name="TranAmount"></param>
+        /// <returns></returns>
+
+      public static string KHKF03(string InAcctNo, string InAcctName,string TranAmount)
+        {
+      string sdbYQAcctNo = PAConfigHelper.GetConfiguration("sdbYQAcctNo");
+            string sdbYQCorpId = PAConfigHelper.GetConfiguration("sdbYQCorpId");
+            string postParams = "{0}";
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<?xml version=\"1.0\" encoding=\"GBK\" ?><Result><OrderNumber>");
+            sb.Append("ZXLKF0320170411TV001");   //20位订单号
+            sb.Append("</OrderNumber>");
+            sb.Append("<AcctNo>");
+            sb.Append(sdbYQAcctNo);   //企业签约帐号
+            sb.Append("</AcctNo>");
+
+            sb.Append("<BusiType>");
+            sb.Append("00000");   
+            sb.Append("</BusiType>");
+
+            //sb.Append("<CorpId>");
+            //sb.Append(sdbYQCorpId);//单位代码
+            //sb.Append("</CorpId>");
+
+            sb.Append("<CcyCode>");
+            sb.Append("RMB");
+            sb.Append("</CcyCode>");
+            sb.Append("<TranAmount>");
+            sb.Append(TranAmount);
+            sb.Append("</TranAmount>");
+
+            sb.Append("<TranAmount>");
+            sb.Append(TranAmount);
+            sb.Append("</TranAmount>");
+
+            sb.Append("<InAcctNo>");
+            sb.Append(InAcctNo);
+            sb.Append("</InAcctNo>");
+
+            sb.Append("<InAcctName>");
+            sb.Append(InAcctName);
+            sb.Append("</InAcctName>");
+
+
+            sb.Append("</Result>");
+
+            string KHKF03XML=string.Format(postParams, sb.ToString());
+            var str = YQHelp.asemblyYQPackets(YQCode, "KHKF03", KHKF03XML);
+
+            var cc = PAHelper.NcPost(apiUrl, str);
+            return cc;
+        }
+
+
+        /// <summary>
+        /// 3.4 单笔付款结果查询[KHKF04]
+        /// </summary>
+        /// <param name="OrderNumber"></param>
+        /// <param name="BussFlowNo"></param>
+        /// <returns></returns>
+        public static string KHKF04(string OrderNumber   , string BussFlowNo = "")
+      {
+          string sdbYQAcctNo = PAConfigHelper.GetConfiguration("sdbYQAcctNo");
+   
+          string postParams = "{0}";
+          StringBuilder sb = new StringBuilder();
+          sb.Append("<?xml version=\"1.0\" encoding=\"GBK\" ?><Result><AcctNo>");
+          sb.Append(sdbYQAcctNo);   //企业签约帐号
+            sb.Append("</AcctNo>");
+          sb.Append("<OrderNumber>");
+          sb.Append(OrderNumber); //20位订单号
+            sb.Append("</OrderNumber>");
+
+
+            //两者不能同时为空    只取上面一个的  BussFlowNo为空
+            sb.Append("<BussFlowNo>");
+            sb.Append(BussFlowNo); //银行业务流水号
+            sb.Append("</BussFlowNo>");
+            sb.Append("</Result>");
+            string KHKF04XML = string.Format(postParams, sb.ToString());
+            var str = YQHelp.asemblyYQPackets(YQCode, "KHKF04", KHKF04XML);
+
+            var cc = PAHelper.NcPost(apiUrl, str);
+            return cc;
+        }
+
+
+      #region 早期测试
+
+
+            //3.1 系统状态探测,没有用封装的post提交_无效
         public static object S001Query()
         {
             string postParams = "{0}";
@@ -122,6 +245,6 @@ namespace sdbBank
             return stringResp;
         }
 
-
+        #endregion
     }
 }
