60d0713727c15af42c77fbdcd5680edd48132ee4
dell <dell@DELL-PC>
2017-04-17 17:28:36 +0800

    model增加

--- sdbBank/YQModel.cs
+++ sdbBank/YQModel.cs
@@ -63,9 +63,107 @@ namespace sdbBank
         public string InAcctName { get; set; }
         public string Mobile { get; set; }
         public string Remark { get; set; }
-  
+
     }
 
 
+
+   
+   
+ 
+    /// <summary>
+    ///当日明细查询结果集返回
+    /// </summary>
+    [XmlRoot("Result")]
+    public class YQ4008Model
+    {
+        public string AcctNo { get; set; }
+        public string CcyCode { get; set; }
+        public string PageNo { get; set; }
+        public string PageSize { get; set; }
+        public string TranDate { get; set; }
+        public string EndFlag { get; set; }
+        public string PageRecCount { get; set; }
+        public string JournalNo { get; set; }
+        public string LogCount { get; set; }
+
+        public listInfos listInfos;
+
+
+    }
+
+
+
+    public class listInfos
+    {
+        [XmlElement("list")]
+        public List<listInfo> listInfoList;
+    }
+
+
+    public class listInfo
+    {
+        [XmlAttribute]
+        public string TranTime1 { get; set; }
+        [XmlAttribute]
+        public string HostSeqNo { get; set; }
+        [XmlAttribute]
+        public string DetailSerialNo { get; set; }
+        [XmlAttribute]
+        public string BussSeqNo { get; set; }
+
+        [XmlAttribute]
+        public string SummonNo { get; set; }
+        [XmlAttribute]
+        public string SendBank { get; set; }
+        [XmlAttribute]
+        public string SendBankNode { get; set; }
+        [XmlAttribute]
+        public string SendAccount { get; set; }
+
+
+
+        [XmlAttribute]
+        public string SendName { get; set; }
+        [XmlAttribute]
+        public string TxAmount { get; set; }
+        [XmlAttribute]
+        public string AcctBank { get; set; }
+        [XmlAttribute]
+        public string AcctBankNode { get; set; }
+
+        [XmlAttribute]
+        public string AcctAccount { get; set; }
+        [XmlAttribute]
+        public string AcctName { get; set; }
+        [XmlAttribute]
+        public string TxType { get; set; }
+        [XmlAttribute]
+        public string AbstractStr { get; set; }
+
+        [XmlAttribute]
+        public string Notes { get; set; }
+        [XmlAttribute]
+        public string Fee1 { get; set; }
+        [XmlAttribute]
+        public string Fee2 { get; set; }
+        [XmlAttribute]
+        public string AbstractStr_Desc { get; set; }
+
+
+        [XmlAttribute]
+        public string CVoucherNo { get; set; }
+        [XmlAttribute]
+        public string CstInnerFlowNo { get; set; }
+        [XmlAttribute]
+        public string TranChannel { get; set; }
+        [XmlAttribute]
+        public string TranCode { get; set; }
+
+        [XmlAttribute]
+        public string HostDate { get; set; }
+    }
+
+  
 
 }
